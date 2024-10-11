a56590f09ce4a47ed115b7b650ac43a82b213e36
fredrikr79 <fredrikrobertsen7@gmail.com>
2024-10-11 23:41:54 +0200

    chore: add `setup.py` with initial info

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+with open("README", 'r') as f:
+    long_description = f.read()
+
+setup(
+   name='freecell',
+   version='1.0',
+   description='Freecell Solitaire in Pygame with Nix',
+   license="GNU GENERAL PUBLIC LICENSE",
+   long_description=long_description,
+   author='fredrikr79',
+   author_email='fredrikrobertsen7@gmail.com',
+   packages=['core', 'ui'],
+   # install_requires=[],  # handled by nix-shell
+   scripts=[]
+)
