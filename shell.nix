024643b5102399a0b2b11b566fdcf90fca592776
fredrikr79 <fredrikrobertsen7@gmail.com>
2024-10-11 23:41:15 +0200

    chore(deps): add setuptools for setup.py packaging

--- shell.nix
+++ shell.nix
@@ -9,5 +9,6 @@ pkgs.mkShell {
         python312Packages.coverage     # test-coverage
         python312Packages.black        # auto-formatting
         python312Packages.flake8       # style and code quality
+        python312Packages.setuptools   # packaging
     ];
 }
